48a14a057e029567c401b4acca64c5302395d553
Gursahaj Singh <gursahajsinghkhalsa@gmail.com>
2024-12-11 03:46:01 +0530

    commit8

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/igloo-clone/",
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-router-dom'],
-    },
-  },
-  resolve: {
-    alias: {
-      react: path.resolve('./node_modules/react'),
-      'react-router-dom': path.resolve('./node_modules/react-router-dom'),
-    },
-  },
 })
